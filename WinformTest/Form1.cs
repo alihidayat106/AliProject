0e808f3009ca2f4a1387f57a4e95024f8a044df4
alihidayat106 <ali.hidayat.106@gmail.com>
2019-01-11 13:06:46 +0700

    Test 01

---
+++ WinformTest/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinformTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("This Part is Association");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int x = 2000;
+            int v = 2000;
+
+            int res = x + v;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("This is testing!");
+        }
+    }
+}
